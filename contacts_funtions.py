c589f67777092ff07eac9ca9e6e76c85e9332ee4
PyTem <temni23@yandex.ru>
2023-08-16 22:24:55 +0700

    Fixed contacts function

--- contacts_funtions.py
+++ contacts_funtions.py
@@ -1,9 +1,8 @@
 import json
 
-
 def save_user_id(user_id: str, full_name: str, username: str) -> None:
-    # Открываем файл contacts.json для чтения
-    with open('contacts/contacts.json', 'r') as contacts:
+    # Открываем файл contacts.json для чтения и записи
+    with open('contacts/contacts.json', 'r+') as contacts:
         try:
             # Загружаем данные из файла в виде словаря
             data = json.load(contacts)
@@ -11,16 +10,23 @@ def save_user_id(user_id: str, full_name: str, username: str) -> None:
             # Обработка ошибки, если файл пуст
             data = {}
 
-    # Проверяем, если user_id уже существует в файле
-    if user_id in data:
-        print(f'Пользователь с ID {user_id} уже существует в файле contacts.json')
-    else:
-        # Добавляем user_id в формате "user_id": user_id в словарь данных
-        data[user_id] = [full_name, username]
+        # Проверяем, если user_id уже существует в файле
+        if user_id in data:
+            print(f'Пользователь с ID {user_id} уже существует в файле contacts.json')
+        else:
+            # Добавляем user_id в формате "user_id": user_id в словарь данных
+            data.update({
+                user_id: {
+                    'fullname': full_name,
+                    'username': username
+                }
+            })
 
-        # Открываем файл contacts.json для записи
-        with open('contacts/contacts.json', 'w') as contacts:
+            # Перемещаем указатель в начало файла
+            contacts.seek(0)
+
             # Записываем обновленные данные обратно в файл
             json.dump(data, contacts, ensure_ascii=False)
+            contacts.truncate()
 
-        print(f'Пользователь с ID {user_id} успешно сохранен в файле contacts.json')
+            print(f'Пользователь с ID {user_id} успешно сохранен в файле contacts.json')
