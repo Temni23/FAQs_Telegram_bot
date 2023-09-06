5d675e77ff0773fa92256020f83cfb1cae8abc6c
Artem Klimov <temni23@yandex.ru>
2023-09-06 10:55:22 +0700

    Fixed PEP8 dictionary_functions.py

--- dictionary_functions.py
+++ dictionary_functions.py
@@ -1,3 +1,5 @@
+"""Содержит функции для работы со словарем ответов."""
+
 import logging
 from logging.handlers import RotatingFileHandler
 
@@ -13,18 +15,22 @@ logger.addHandler(handler)
 
 def serch_key_by_part(dictionary: dict, part: str) -> str or dict:
     """Ищет в словаре значение по первым 32 символам ключа.
-    32 символа связаны с ограничением callback_data в InlineButton"""
+
+    32 символа связаны с ограничением callback_data в InlineButton
+    """
     logger.debug(
         f'Пробую найти ответ на запрос {part} функция serch_key_by_part')
     for key in dictionary.keys():
         if part == key[:32]:
             logger.debug(
-                f'Возвращаю ответ {dictionary[key]} на запрос {part} функция serch_key_by_part')
+                f'Возвращаю ответ {dictionary[key]} на запрос {part}'
+                f'функция serch_key_by_part')
             return dictionary[key]
     for key, value in dictionary.items():
         if isinstance(value, dict):
             result = serch_key_by_part(value, part)
             if result is not None:
                 logger.debug(
-                    f'Возвращаю ответ {result} на запрос {part} функция serch_key_by_part')
+                    f'Возвращаю ответ {result} на запрос {part}'
+                    f'функция serch_key_by_part')
                 return result
